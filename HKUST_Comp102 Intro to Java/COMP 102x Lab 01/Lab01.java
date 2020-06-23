089f58bbec9bf27585ac91544da296294975adba
”Ran” <ran@github.com>
2020-06-23 23:14:47 +1000

    Java Intro

---
+++ HKUST_Comp102 Intro to Java/COMP 102x Lab 01/Lab01.java
@@ -0,0 +1,10 @@
+
+public class Lab01
+{
+   public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("Hello, everyone!");
+        System.out.println("全世界，大家好！");
+
+   }
+}
